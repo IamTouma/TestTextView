2cb391471ed4e3db2a13a4d658b966b2f939c91c
IamTouma <IamTouma@gmail.com>
2018-04-23 09:50:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/testtextview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.testtextview;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text_view);
+
+        // resourceに設定が必要
+        textView.setText(R.string.text);
+
+    }
+}
